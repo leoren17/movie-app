6136d4e4bd118cb81fce2de388701859238c71b5
Leo <leojy.ren@gmail.com>
2023-07-30 16:49:10 -0400

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 import './app.css';
 
+// omdb api key: e957e0d2
+const OMDB_API_URL = "http://www.omdbapi/?apikey=e957e0d2";
+
 const App = () => {
+
+    // search for movies
+    const search = async (title) => {
+        const responseObject = await fetch(`${OMDB_API_URL}&s=${title}`);
+        const data = await responseObject.json();
+        console.log(data);
+    }
+    // same as componentDidMount()
+    useEffect(() => {
+        search("pulp");
+    }, []);
+
     return (
         <>
             <h1>Movie App</h1>
