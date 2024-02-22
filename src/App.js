6cb3d596fb9a6320771e397778a629f41ee9a515
Leo Ren <leojy.ren@gmail.com>
2024-02-22 15:14:05 -0500

    improved ui

--- src/App.js
+++ src/App.js
@@ -1,73 +1,94 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './app.css';
-import MovieCard from './MovieCard';
+import React from "react";
+import { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./app.css";
+import MovieCard from "./MovieCard";
 
 // omdb api key: e957e0d2
 const OMDB_API_URL = "http://www.omdbapi.com/?apikey=e957e0d2";
 
 const App = () => {
-    const [searchTerm, setSearchTerm] = useState('');
-    const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [total, setTotal] = useState(0);
 
-    // search for movies
-    const search = async (title) => {
-        const responseObject = await fetch(`${OMDB_API_URL}&s=${title}`);
-        const data = await responseObject.json();
-        if (data.Search) {
-            setMovies(data.Search);
-        }        
+  // search for movies
+  const search = async (title) => {
+    const responseObject = await fetch(`${OMDB_API_URL}&s=${title}`);
+    const data = await responseObject.json();
+    // console.log(data);
+
+    setMovies(data.Search);
+
+    if (data.totalResults) {
+      setTotal(data.totalResults);
+    } else {
+      setTotal(0);
     }
-    // same as componentDidMount()
-    useEffect(() => {
-        search("avengers");
-    }, []);
+  };
+  // same as componentDidMount()
+  useEffect(() => {
+    search("avengers");
+  }, []);
 
-    // each time searchTerm changes
-    useEffect(() => {
-        search(searchTerm);
-    }, [searchTerm]);
+  // each time searchTerm changes
+  useEffect(() => {
+    search(searchTerm);
+  }, [searchTerm]);
 
-    // what an entry in data.Search looks like
-    const tmp = {
-        "Title": "Pulp Fiction",
-        "Year": "1994",
-        "imdbID": "tt0110912",
-        "Type": "movie",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-      }
+  // what an entry in data.Search looks like
+  const tmp = {
+    Title: "Pulp Fiction",
+    Year: "1994",
+    imdbID: "tt0110912",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+  };
 
-    return (
-        <div className="app">
-            <div className="container-fluid">      
-                <div className="row align-items-center m-4">
-                    <h1 className="mb-3 text-center">Search for Movies</h1>
-                    <input className="col form-control input"
-                        placeholder="Search for movies"
-                        value={searchTerm}
-                        onChange={(e) => {setSearchTerm(e.target.value)}}
-                    />
-                    {/* <button className="col"
+  return (
+    <div className="app">
+      <div className="container-fluid">
+        <div className="row align-items-center m-5">
+          <h1 className="mb-3 text-center">Search for Movies</h1>
+          <input
+            className="col form-control-lg"
+            placeholder="Search for movies"
+            value={searchTerm}
+            onChange={(e) => {
+              setSearchTerm(e.target.value);
+            }}
+          />
+          {/* <button className="col"
                         onClick={() => {search(searchTerm)}}>
                         search!
                     </button> */}
-                </div>           
-            {
-                // checks to make sure movies is not null or undefined
-                // if movies is null/undefined, return undefined
-                movies?.length > 0 
-                ? (                    
-                    <div className='row'>
-                        {movies.map((m) => <MovieCard movie = {m}/>)}
-                    </div>
-                ) : (
-                    <h1>No Movies Found!</h1>
-                )
-            }        
-            </div>
         </div>
-    );
-}
+
+        <div className="mb-3 text-center">
+          <h2>
+            Total Movies Found: <i>{total}</i>
+          </h2>
+        </div>
+
+        {
+          // checks to make sure movies is not null or undefined
+          // if movies is null/undefined, return undefined
+          movies?.length > 0 ? (
+            <div className="row align-items-center">
+              {movies.map((m) => (
+                <MovieCard movie={m} />
+              ))}
+            </div>
+          ) : (
+            <div className="mb-3 text-center">
+              <h1>No Movies Found!</h1>
+            </div>
+          )
+        }
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './moviecard.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./moviecard.css";
 
-const MovieCard = ({movie}) => {
-    return (
-        
-        <div className='col m-2 d-flex justify-content-start'>
-            {/* <div>
+const MovieCard = ({ movie }) => {
+  return (
+    <div className="col m-2 d-flex justify-content-center">
+      {/* <div>
                 <h3>{movie.Title}</h3>
             </div>
             <div>
@@ -15,13 +14,25 @@ const MovieCard = ({movie}) => {
             <div>
                 <p>{movie.Type}</p>
             </div> */}
-            <div className="img-container">
-                {/* <img src='https://via.placeholder.com/400' /> */}
-                <img className="poster"
-                    src={movie.Poster === 'N/A' ? 'https://placehold.co/270x400?text=Poster+N/A' : movie.Poster} alt={movie.Title} />
-            </div>
+      <div className="img-container">
+        {/* <img src='https://via.placeholder.com/400' /> */}
+        <img
+          className="poster"
+          src={
+            movie.Poster === "N/A"
+              ? "https://placehold.co/270x400?text=Poster+N/A"
+              : movie.Poster
+          }
+          alt={movie.Title}
+        />
+        <div></div>
+        <div>
+          <h3>{movie.Title}</h3>
+          <p>{movie.Year}</p>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default MovieCard;
